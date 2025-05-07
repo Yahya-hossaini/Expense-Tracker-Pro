e76ea7f7b1c344c4cea07159294960d37a25694a
Yahya Hossaini <yahyahossaini2022@gmail.com>
2025-05-07 10:47:10 +0430

    adding the necessery base contents for home screen:
    no design, only contents.
    using dummy data for listview.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,57 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> transaction = [
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+    {
+      'icon': Icons.fastfood,
+      'title': 'Title',
+      'amount': 99,
+      'date': DateTime.now(),
+    },
+  ];
+
   bool isDarkMode = false;
 
   void toggleTheme() {
@@ -18,13 +69,89 @@ class _HomeScreenState extends State<HomeScreen> {
       isDarkMode = !isDarkMode;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: CustomDrawer(),
-      appBar: CustomAppBar(isDarkMode: isDarkMode, toggleTheme: toggleTheme, ),
-      body: Center(
-        child: Image.asset('assets/images/coin.png'),
+      appBar: CustomAppBar(
+        isDarkMode: isDarkMode,
+        toggleTheme: toggleTheme,
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Column(
+            children: [
+              Text('Home'),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.green,
+                      padding: EdgeInsets.all(12),
+                      margin: EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Total'),
+                          Center(child: Text('\$1500')),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.green,
+                      padding: EdgeInsets.all(12),
+                      margin: EdgeInsets.all(12),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Remaining'),
+                          Center(child: Text('\$600')),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text('Recent Transaction:'),
+              Divider(
+                thickness: 2,
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: transaction.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.lightBlueAccent,
+                      width: double.infinity,
+                      margin: EdgeInsets.all(12),
+                      padding: EdgeInsets.all(12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(transaction[index]['icon']),
+                              Column(
+                                children: [
+                                  Text(transaction[index]['title']),
+                                  Text('${transaction[index]['amount']}'),
+                                ],
+                              ),
+                            ],
+                          ),
+                          Text('${transaction[index]['date']}'),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
