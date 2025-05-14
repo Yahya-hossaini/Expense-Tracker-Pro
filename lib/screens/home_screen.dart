7bc5a22d49a3f8ac6857ba5ec538f6149c55fce3
Yahya Hossaini <yahyahossaini2022@gmail.com>
2025-05-14 14:07:30 +0430

    working on responsiveness phase 2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -96,7 +96,7 @@ class _HomeScreenState extends State<HomeScreen> {
         toggleTheme: toggleTheme,
       ),
       body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
+        builder: (context, constrains) {
           return Padding(
             padding: EdgeInsets.symmetric(
               horizontal: size.width * 0.06,
@@ -106,214 +106,205 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  flex: 40,
+                  flex: 35,
+                  child: Container(
+                    color: Colors.green,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Home',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(
-                      height: size.height * 0.005,
-                    ),
-                    Row(
                       children: [
                         Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(12),
-                            margin: const EdgeInsets.symmetric(vertical: 12),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.3),
-                                  spreadRadius: 2,
-                                  blurRadius: 2,
-                                  offset: const Offset(0, 2),
-                                ),
-                              ],
-                              color: Colors.black.withOpacity(0.7),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Total',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize:
-                                          MediaQuery.of(context).size.width *
-                                              0.04,
-                                      color: Colors.green),
-                                ),
-                                Center(
-                                  child: FittedBox(
-                                    fit: BoxFit.scaleDown,
-                                    child: Text(
-                                      '\$1500',
-                                      style: TextStyle(
-                                          fontSize:
-                                              MediaQuery.of(context).size.width *
-                                                  0.08,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.green),
-                                    ),
+                          flex: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                'Home',
+                                style: TextStyle(
+                                    fontSize: size.width * 0.06,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  const Expanded(
+                                    child: TotalRemainingCard(title: 'Total', amount: 1500,),
                                   ),
-                                ),
-                              ],
-                            ),
+                                  SizedBox(
+                                    width: size.width * 0.06,
+                                  ),
+                                  const Expanded(
+                                    child: TotalRemainingCard(title: 'Remaining', amount: 600,),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
-                        const SizedBox(
-                          width: 24,
-                        ),
                         Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.all(12),
-                            margin: const EdgeInsets.symmetric(vertical: 12),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.black.withOpacity(0.3),
-                                  spreadRadius: 2,
-                                  blurRadius: 2,
-                                  offset: const Offset(0, 2),
+                          flex: 1,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              FittedBox(
+                                child: Text(
+                                  'Recent Transaction:',
+                                  style:
+                                  TextStyle(fontSize: size.height * 0.018, fontWeight: FontWeight.w500),
                                 ),
-                              ],
-                              color: Colors.black.withOpacity(0.7),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Remaining',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize:
-                                          MediaQuery.of(context).size.width *
-                                              0.04,
-                                      color: Colors.green),
-                                ),
-                                Center(
-                                  child: FittedBox(
-                                    fit: BoxFit.scaleDown,
-                                    child: Text(
-                                      '\$600',
-                                      style: TextStyle(
-                                          fontSize:
-                                              MediaQuery.of(context).size.width *
-                                                  0.08,
-                                          fontWeight: FontWeight.w500,
-                                          color: Colors.green),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
+                              ),
+                              Divider(
+                                thickness: 2,
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                    Text(
-                      'Recent Transaction:',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-                    ),
-                    Divider(
-                      thickness: 2,
-                    ),
-                  ],
-                )),
+                  ),
+                ),
                 Expanded(
-                  flex: 80,
-                  child: ListView.builder(
-                    itemCount: transaction.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        decoration: BoxDecoration(
-                          color: Colors.blueGrey[600],
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.3),
-                              spreadRadius: 2,
-                              blurRadius: 2,
-                              offset: const Offset(0, 2),
-                            ),
-                          ],
-                        ),
-                        width: double.infinity,
-                        margin: EdgeInsets.symmetric(
-                            vertical: constraints.maxHeight * 0.005,
-                            horizontal: 2),
-                        padding: EdgeInsets.all(constraints.maxHeight * 0.015),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Expanded(
-                              flex: 1,
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Icon(
-                                    transaction[index]['icon'],
-                                    color: getIconColor(
-                                        transaction[index]['icon']),
-                                    size: constraints.maxHeight * 0.05,
-                                  ),
-                                  SizedBox(width: constraints.maxHeight * 0.02),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        transaction[index]['title'],
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            color: Colors.white70,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                      const SizedBox(
-                                        height: 4,
-                                      ),
-                                      Text(
-                                        '\$${transaction[index]['amount']}',
-                                        style: const TextStyle(
-                                            fontSize: 16,
-                                            color: Colors.greenAccent,
-                                            fontWeight: FontWeight.w500),
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                  flex: 65,
+                  child: Container(
+                    color: Colors.blueAccent,
+                    child: ListView.builder(
+                      itemCount: transaction.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blueGrey[600],
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.3),
+                                spreadRadius: 2,
+                                blurRadius: 2,
+                                offset: const Offset(0, 2),
                               ),
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: Container(
-                                alignment: Alignment.centerRight,
-                                child: Text(
-                                  'Date: ${intl.DateFormat('MM/dd/yyyy').format(transaction[index]['date'])}',
-                                  style: TextStyle(
-                                      color: Colors.white70,
-                                      fontWeight: FontWeight.w500),
+                            ],
+                          ),
+                          width: double.infinity,
+                          margin: EdgeInsets.symmetric(
+                              vertical: constrains.maxHeight * 0.005,
+                              horizontal: 2),
+                          padding: EdgeInsets.all(constrains.maxHeight * 0.015),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Icon(
+                                      transaction[index]['icon'],
+                                      color: getIconColor(
+                                          transaction[index]['icon']),
+                                      size: constrains.maxHeight * 0.05,
+                                    ),
+                                    SizedBox(width: constrains.maxHeight * 0.02),
+                                    Column(
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          transaction[index]['title'],
+                                          style: TextStyle(
+                                              fontSize: size.height * 0.021,
+                                              color: Colors.white70,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        const SizedBox(
+                                          height: 4,
+                                        ),
+                                        Text(
+                                          '\$${transaction[index]['amount']}',
+                                          style: TextStyle(
+                                              fontSize: size.height * 0.02,
+                                              color: Colors.greenAccent,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ),
-                          ],
-                        ),
-                      );
-                    },
+                              Expanded(
+                                flex: 3,
+                                child: Container(
+                                  alignment: Alignment.centerRight,
+                                  child: Text(
+                                    'Date: ${intl.DateFormat('MM/dd/yyyy').format(transaction[index]['date'])}',
+                                    style: TextStyle(
+                                        color: Colors.white70,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ],
             ),
           );
         },
+      ),
+    );
+  }
+}
+
+class TotalRemainingCard extends StatelessWidget {
+  final String title;
+  final int amount;
+
+  const TotalRemainingCard({
+    super.key,
+    required this.title,
+    required this.amount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.all(size.height * 0.01),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 2,
+            offset: const Offset(0, 2),
+          ),
+        ],
+        color: Colors.black.withOpacity(0.7),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: size.height * 0.015,
+                color: Colors.green),
+          ),
+          Center(
+            child: Text(
+              '\$$amount',
+              style: TextStyle(
+                  fontSize: size.height * 0.03,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.green),
+            ),
+          ),
+        ],
       ),
     );
   }
